ee4b36426b87eb0393f3846c62dbeaa9ebf5accb
3103119100@student.smktelkom-pwt.sch.id <3103119100@student.smktelkom-pwt.sch.id>
2021-08-29 17:46:01 +0700

    Migration

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthController;
+use App\Http\Controllers\BookController;
+use GuzzleHttp\Promise\create;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +17,14 @@ use App\Http\Controllers\AuthController;
 |
 */
 
-Route::get('/me', [AuthController::class, 'me']);
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+// Route::get('/book',[BookController::class, 'index']);
+// Route::post('/book',[BookController::class, 'store']);
+// Route::get('/book/{id}',[BookController::class, 'show']);
+// Route::put('/book/{id}',[BookController::class, 'update']);
+// Route::delete('/book/{id}',[BookController::class, 'destroy']);
+
+Route::resource('Book',BookController::class)->except('create','edit');
