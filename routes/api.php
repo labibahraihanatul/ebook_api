e31f544d90ed40732dd10cf3aa3dfb7256bb0fa0
3103119100@student.smktelkom-pwt.sch.id <3103119100@student.smktelkom-pwt.sch.id>
2021-10-09 23:42:24 +0700

    auth

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BookController;
-
+use App\Http\Controllers\AuthorController;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,9 +20,33 @@ use App\Http\Controllers\BookController;
 |
 */
 
+Route::get('/me', [AuthController::class, 'me']);
 Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+  return $request->user();
 });
 
-Route::resource("books", "BookController");
-Route::resource("authors", "AuthorController");
+//  login register
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+/* Authors Books*/
+// public
+Route::get('/books', [BookController::class, 'index']);
+Route::get('/books/{id}', [BookController::class, 'show']);
+Route::get('/books/search/{title}', [BookController::class, 'search']);
+
+Route::get('/authors', [AuthorController::class, 'index']);
+Route::get('/authors/{id}', [AuthorController::class, 'show']);
+Route::get('/authors/search/{name}', [AuthorController::class, 'search']);
+
+// protected
+Route::middleware(['auth:sanctum'])->group(function () {
+  Route::post('/books', [BookController::class, 'store']);
+  Route::put('/books/{id}', [BookController::class, 'update']);
+  Route::delete('/books/{id}', [BookController::class, 'destroy']);
+
+  Route::post('/authors', [AuthorController::class, 'store']);
+  Route::put('/authors/{id}', [AuthorController::class, 'update']);
+  Route::delete('/authors/{id}', [AuthorController::class, 'destroy']);
+  Route::post('/logout', [AuthController::class, 'logout']);
+});
